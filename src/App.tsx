253ddf1e0275c9f21bed4628100ff7341498d86a
Vinicius de Moraes <vimoraescarvalho@gmail.com>
2021-03-16 19:16:26 -0300

    Create dashboard components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import { GlobalStyle } from "./styles/global";
 
+import { Header } from "./components/Header";
+import { Dashboard } from "./components/Dashboard";
+
 export function App() {
   return (
-    <div className="App">
-      <h1>Hello world</h1>
+    <>
+      <Header />
+      <Dashboard />
       <GlobalStyle />
-    </div>
+    </>
   );
 }
